25b35501b905f5a02334a12dd58b4216395e0e5b
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-10-02 12:30:28 -0500

    Add files via upload
    el tkinter

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+ventana = tk.Tk()
+ventana.title("Mi Ventana")
+ventana.geometry("300x200")
+
+etiqueta = tk.Label(ventana, text="¡Hola, mundo!")
+etiqueta.pack()
+
+boton = tk.Button(ventana, text="Haz clic")
+boton.pack()
+
+ventana.mainloop()
