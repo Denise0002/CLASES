ff15434b0c9148983ec7d9542247f00a4553b0e6
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-10-23 17:15:44 -0500

    Update nueva_interfaz_five.py

--- INTERFACES_HDS/nueva_interfaz_five.py
+++ INTERFACES_HDS/nueva_interfaz_five.py
@@ -1,15 +1,59 @@
-from tkinter import *
-ventana= Tk()
-ventana.geometry("300x350")
-ventana.title("ventana suma")
+# from tkinter import *
+# ventana= Tk()
+# ventana.geometry("300x350")
+# ventana.title("ventana suma")
 
-etiqueta=Label(ventana,text=("introduce tu nombre"))
-# etiqueta.grid(row=,column=)
-etiqueta.pack()
+# def  captura_dato():
+#     text=text_nombre.get()
+#     mensaje=Label(ventana,text=f"hola,{text}")
+#     mensaje.pack()
 
-text_nombre=Entry(ventana)
-text_nombre.config(bg="blue",fg=("yellow"))+
-text_nombre.pack()
+# etiqueta=Label(ventana,text=("introduce tu nombre"))
+# # etiqueta.grid(row=,column=)
+# etiqueta.pack()
+
+# text_nombre=Entry(ventana)
+# text_nombre.config(bg="blue",fg="yellow")
+# text_nombre.pack()
+
+# boton_capturar=Button(ventana,
+# text="Enviar",command=captura_dato)
+# boton_capturar.pack()
+
+# ventana.mainloop()
+
+# from tkinter import *
+# ventana= Tk()
+# ventana.geometry("300x350")
+# ventana.title("Mobile Legends")
+
+# etiqueta=Label(ventana,text=("Usurio:"))
+# etiqueta.config(bg="white",fg="blue")
+# etiqueta.pack()
+
+# text_nombre=Entry(ventana)
+# text_nombre.config(bg="white",fg="black")
+# text_nombre.pack()
+
+# etiqueta=Label(ventana,text=("Contraseña:"))
+# etiqueta.config(bg="white",fg="blue")
+# etiqueta.pack()
+
+# text_nombre=Entry(ventana)
+# text_nombre.config(bg="white",fg="black")
+# text_nombre.pack()
+
+# def  captura_dato():
+#     text=text_nombre.get()
+#     mensaje=Label(ventana,text=f"Bienvenido a Mobile Legends")
+#     mensaje.pack()
 
 
-ventana.mainloop()
+# boton_capturar=Button(ventana,
+# text="Iniciar Secion",command=captura_dato, bg="green",fg="white")
+# boton_capturar.pack()
+
+
+# ventana.mainloop()
+
+
