628d3a43c93564021208d72f8edc14eb53fe11b1
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-10-24 15:23:54 -0500

    Add files via upload

---
+++ INTERFACES_HDS/bd.py
@@ -0,0 +1,33 @@
+negocios=[
+    {
+        "ruc":1234,
+        "nombre":"el crack",
+        "categoria":["bodega","farmacia","bar"],
+        "horario_atencion":{
+            "dia":"7am-12pm",
+            "tarde":"2pm-8pm,"
+        },
+        "gerente":"china"
+    },
+    {
+        "ruc":123456789,
+        "nombre":"el crack",
+        "categoria":["bodega","cevicheria","telefonicaS"],
+        "horario_atencion":{
+            "dia":"7am-12pm",
+            "tarde":"2pm-8pm,"
+        },
+        "gerente":"nadine"
+    },
+    {
+        "ruc":857496321,
+        "nombre":"el crack",
+        "categoria":["bodega"],
+        "horario_atencion":{
+            "dia":"7am-12pm",
+            "tarde":"2pm-8pm,"
+        },
+        "gerente":"manuela"
+    }
+]
+
