df9c6d3d8156341645533a22d2b97651932d3a0e
Denise0002 <132002183+Denise0002@users.noreply.github.com>
2023-11-06 13:51:59 -0500

    funciones de una calculadora

--- INTERFACES_HDS/calculadora/funciones.py
+++ INTERFACES_HDS/calculadora/funciones.py
@@ -1,2 +1,30 @@
+from tkinter import*
 def enviar_boton(valor,pantalla):
-    pantalla.insert(0,valor)
+    anterior = pantalla.get()
+    pantalla.delete(0,END)
+    pantalla.insert(0,str(anterior)+str(valor))
+
+def operacion(signo,pantalla):
+    global num1
+    global signo_op
+    num1=pantalla.get()
+    num1=float(num1)
+    pantalla.delete(0,END)
+    signo_op=signo
+
+def igual(pantalla):
+    global num2
+    num2=pantalla.get()
+    pantalla.delete(0,END)
+    match signo_op:
+        case "+":
+            pantalla.insert(0, num1 + float(num2))
+        case "-":
+            pantalla.insert(0, num1 - float(num2))
+        case "*":
+            pantalla.insert(0, num1 * float(num2))
+        case "/":
+            pantalla.insert(0, num1 // float(num2))
+
+def limpiar(pantalla):
+    pantalla.delete(0,END)
